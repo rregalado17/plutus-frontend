6b30b1b3607a82ab7804f396b8d1eadc864b9cdb
rregalado17 <rregalado17@gmail.com>
2021-05-25 23:15:43 -0400

    add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ( {title}) => {
+    
+    const onClick = () => {
+        console.log('Click')
+    }
+    
     return (
-        <header>{title}</header>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Buy Cryptocurrency' onClick={onClick}/>
+        </header>
     )
 }
 
