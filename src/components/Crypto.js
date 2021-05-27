59b442e438c44926a6fffc65ab87f25752fe0eb5
rregalado17 <rregalado17@gmail.com>
2021-05-26 23:06:17 -0400

    sell button

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,10 +1,12 @@
-const crypto = ( {crypto} ) => {
+import { FaTimes } from 'react-icons/fa'
+
+const Crypto = ( {crypto} ) => {
     return (
         <div className='crypto'>
-            <h3>{crypto.name}</h3>
+            <h3>{crypto.name} <FaTimes style={{color: 'red', cursor: 'pointer' }} /> </h3>
             <p>{crypto.price}</p>
         </div>
     )
 }
 
-export default crypto
+export default Crypto
