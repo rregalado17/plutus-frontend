6c2925415b54e77ee8e44072dc2295300c9039cd
rregalado17 <rregalado17@gmail.com>
2021-06-01 23:28:11 -0400

    build login form

---
+++ src/components/Login.js
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+
+const Login = ( {onLogin}) => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!username) {
+            alert('Please enter your username')
+            return 
+        }
+
+        onLogin({ username, password })
+            setUsername('')
+            setPassword('')
+    }
+
+
+    return (
+        <form className='add-form' onSubmit={onSubmit}>
+            <div className='form-control'>
+                <label>Username</label>
+                <input 
+                    type='text' 
+                    placeholder=''
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)} 
+                />
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Password</label>
+                <input 
+                    type='text' 
+                    placeholder='Password' 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
+            </div>
+            {/* <div className='form-control'>
+                <label>Active Crypto</label>
+                <input 
+                    type='checkbox'
+                    checked={active}
+                    value={active}
+                    onChange={(e) => setActive(e.currentTarget.checked)} 
+                />
+            </div> */}
+        
+            <input type='submit' value='Login to your Account' className='btn btn-block' /> 
+            
+        </form>
+    )
+}
+
+
+
+// const Login = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from './components/Login'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
